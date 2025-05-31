06c4317d21e698d85755d746937f2d9751592ed1
Piyush Prajapati <168058069+Prajapati-Piyush@users.noreply.github.com>
2025-05-31 17:09:04 +0530

    Update Image.jsx

--- client/src/components/Image.jsx
+++ client/src/components/Image.jsx
@@ -1,7 +1,7 @@
 export default function Image({ src, ...rest }) {
     src = src && src.includes('https://')
         ? src
-        : 'https://villa-vista.onrender.comuploads/' + src;
+        : 'https://villa-vista.onrender.com/uploads/' + src;
     return (
         <img {...rest} src={src} alt={''} />
     );
