e8a8064368c417f10d9475f9ddde6f7af397251d
Piyush Prajapati <168058069+Prajapati-Piyush@users.noreply.github.com>
2025-05-31 15:57:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -211,7 +211,13 @@ app.post('/login', async (req, res) => {
                 if (err) {
                     return res.status(500).json({ message: 'Error generating token' });
                 }
-                res.cookie('token', token).json(userDoc);
+                // res.cookie('token', token).json(userDoc);
+                res.cookie('token', token, {
+                    httpOnly: true,
+                    secure: true,
+                    sameSite: 'none',
+                }).json(userDoc);
+
             }
         );
     } catch (e) {
