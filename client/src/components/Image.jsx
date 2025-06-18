04c30c0c44b676336338afdf9281c6a262286b15
Piyush Prajapati <168058069+Prajapati-Piyush@users.noreply.github.com>
2025-06-18 20:25:01 +0530

    Update Image.jsx

--- client/src/components/Image.jsx
+++ client/src/components/Image.jsx
@@ -1,8 +1,11 @@
 export default function Image({ src, ...rest }) {
-    src = src && src.includes('https://')
-        ? src
-        : 'http://localhost:3000/uploads/' + src;
-    return (
-        <img {...rest} src={src} alt={''} />
-    );
+  const isFullUrl = src?.startsWith("http://") || src?.startsWith("https://");
+
+  const backendBaseUrl = import.meta.env.MODE === 'development'
+    ? 'http://localhost:3000'
+    : 'https://villa-vista-backend.onrender.com';
+
+  const finalSrc = isFullUrl ? src : `${backendBaseUrl}/uploads/${src}`;
+
+  return <img {...rest} src={finalSrc} alt="" />;
 }
